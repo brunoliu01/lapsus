41945ac35a36c0a19df160772829e158096cb855
BrunoLiu <bliu@atypax.com>
2017-11-10 17:20:09 -0500

    gracias mundo :)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-$(".slideshow").slick({
-	infinite: true,
-  dots: true,
-   slidesToScroll: 1,
-  autoplay: true,
- autoplaySpeed: 3000
+$(document).ready(function(){
+	$(".slideshow").slick({
+		infinite: true,
+	    dots: true,
+	    slidesToScroll: 1,
+	    autoplay: true,
+	    autoplaySpeed: 3000
+	});
+
+	$(".hamburguer").click(function(){
+		$(".navegation-2 ul").toggleClass("trans");
+	});
 });
+
+
