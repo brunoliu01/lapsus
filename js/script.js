674200980484533c2719563882f8f058966ceaaa
BrunoLiu <bliu@atypax.com>
2017-11-13 17:17:20 -0500

    agrego scroll

--- js/script.js
+++ js/script.js
@@ -8,15 +8,30 @@ $(document).ready(function(){
 	    autoplay: true,
 	    autoplaySpeed: 3000
 	});
+	var a = $('.cont-slider').height();
 //este script sirve para hacer el menu hamburguesa
 //"hamburguer" es el ícono al que le daré clic y "navegation-2 ul" es a lo que le daré la nueva clase "trans"
 	$(".hamburguer").click(function(){
 		$(".navegation-2 ul").toggleClass("trans");
 	});
-});
 
+	$('.novedades-section').click(function(){
+		$("html,body").animate({scrollTop: a + $('.serv').height() + $('.section-1').height()  +$('.parallax').height() + $('.enfoque-text').height() + $('.enfoque').height() -78 },600);
+	});
 
- $('.serviciosSection').click(function(){
-    $("html, body").animate({ scrollTop: $('.cont-slider').height() - 78 }, 600);
+	$('.serviciosSection').click(function(){
+    	$("html, body").animate({ scrollTop: a - 78 }, 600);
+	});
 
+	$('.client-section').click(function(){
+	    $("html,body").animate({scrollTop: a + $('.serv').height() + $('.section-1').height()  +$('.parallax').height() + $('.enfoque-text').height() + $('.enfoque').height() + $('.nov-section').height() -78 },600);
+	});
+
+	$('.contacto-section').click(function(){
+	    $("html,body").animate({scrollTop: a + $('.serv').height() + $('.section-1').height()  +$('.parallax').height() + $('.enfoque-text').height() + $('.enfoque').height() + $('.nov-section').height() + $('.client-text').height() + $('.clientes').height() -78 },600);
+	});
+
+	$('.nosotros-section').click(function(){
+	    $("html,body").animate({scrollTop: a + $('.serv').height() + $('.section-1').height()  +$('.parallax').height() + $('.enfoque-text').height() + $('.enfoque').height() + $('.nov-section').height() + $('.client-text').height() + $('.clientes').height() + $('.section-contact').height() -78 },600);
+	});
 });
