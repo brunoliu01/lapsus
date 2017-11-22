a63c4220daee62e9ad8efd6c80ed375881f9baa3
BrunoLiu <bliu@atypax.com>
2017-11-22 10:37:06 -0500

    continuan los cambios del responsive

--- js/script.js
+++ js/script.js
@@ -2,13 +2,6 @@
 //este código es para hacer el slider con slick
 
 $(document).ready(function(){
-	$(".slideshow").slick({
-		infinite: true,
-	    dots: true,
-	    slidesToScroll: 1,
-	    autoplay: true,
-	    autoplaySpeed: 3000
-	});
 	var a = $('.cont-slider').height();
 //este script sirve para hacer el menu hamburguesa
 //"hamburguer" es el ícono al que le daré clic y "navegation-2 ul" es a lo que le daré la nueva clase "trans"
@@ -31,5 +24,14 @@ $(document).ready(function(){
 	$('.contacto-section').click(function(){
 	    $("html,body").animate({scrollTop: a + $('.serv').height() + $('.section-1').height()  +$('.parallax').height() + $('.enfoque-text').height() + $('.enfoque').height() + $('.nov-section').height() + $('.client-text').height() + $('.clientes').height() -78 },600);
 	});
+	
+	$(".slideshow").slick({
+		infinite: true,
+	    dots: true,
+	    slidesToScroll: 1,
+	    autoplay: true,
+	    autoplaySpeed: 3000
+	});
+	
 
 });
